1bd6d0de9a4956460fe8d7a940e5241e147d57c7
Vinícius Gusmão <vigusmao@gmail.com>
2018-05-11 09:58:21 -0300

    encontrar par

--- JavaUtil/src/com/company/Main.java
+++ JavaUtil/src/com/company/Main.java
@@ -4,8 +4,9 @@ import java.util.*;
 
 public class Main {
 
-    private static final int N = 20000;
+    private static final int N = 160_000;
     private static final int MAX_VALUE = 1_000_000;
+    private static final int SOMA_DESEJADA = -1;
 
     private static void rodarTeste(Collection<Integer> lista, String nomeColecao) {
         long inicio = System.currentTimeMillis();
@@ -22,6 +23,31 @@ public class Main {
                 nomeColecao, contImpares, duracao));
     }
 
+    /**
+     * Enontra dois inteiros na lista dada que somem o valor desejado.
+     *
+     * @param lista Lista de inteiros
+     * @param somaDesejada A soma desejada
+     * @return Um dos inteiros encontrados, caso exista um par que some o valor desejado;
+     *         null, caso contrario.
+     */
+    public static Integer encontrarPar(List<Integer> lista, int somaDesejada) {
+        for (int i = 0; i < lista.size(); i++) {
+            int elemento = lista.get(i);
+            
+//            for (int j = i+1; j < lista.size(); j++) {
+//                int outro = lista.get(j);
+//                if (elemento + outro == somaDesejada) {
+//                    return elemento;
+//                }
+//            }
+            int complementar = somaDesejada - elemento;
+            if (lista.contains(complementar)) {
+                return elemento;
+            }
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
 
@@ -33,13 +59,13 @@ public class Main {
             lista.add(elemento);
         }
 
-        Set<Integer> conjunto = new HashSet<>();
-        while (conjunto.size() < N) {
-            int elemento = random.nextInt(MAX_VALUE);
-            conjunto.add(elemento);
-        }
+        long inicio = System.currentTimeMillis();
+        Integer resultado = encontrarPar(lista, SOMA_DESEJADA);
+        long duracao = System.currentTimeMillis() - inicio;
+        System.out.println(resultado == null ? "Nao encontrei" :
+                String.format("Encontrei um par: %d, %d",
+                        resultado, SOMA_DESEJADA - resultado));
+        System.out.println("Duracao = " + duracao);
 
-        rodarTeste(lista, "Lista");
-        rodarTeste(conjunto, "Conjunto");
     }
 }
